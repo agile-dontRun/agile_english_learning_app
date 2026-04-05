dc8f65f1af947a995eb3d49f41751f13c394d30d
blinder-l <txwd.ch@gmail.com>
2026-04-05 22:35:09 +0800

    DB connection code:Module 3: Verify whether the user is logged in

--- galgame/api/get_tutorial_status.php
+++ galgame/api/get_tutorial_status.php
@@ -1,3 +1,10 @@
 <?php
 session_start();
 header('Content-Type: application/json; charset=utf-8');
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode([
+        'success' => false,
+        'message' => '未登录'
+    ]);
+    exit;
+}
