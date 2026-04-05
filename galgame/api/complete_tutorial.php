57f0264a26f4e440217e33f7b0ce6f47bafc96ce
blinder-l <txwd.ch@gmail.com>
2026-04-05 22:46:38 +0800

    DB part2: Module 3: Verify whether the user is logged in (security interception)

--- galgame/api/complete_tutorial.php
+++ galgame/api/complete_tutorial.php
@@ -1,2 +1,9 @@
 session_start();
 header('Content-Type: application/json; charset=utf-8');
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode([
+        'success' => false,
+        'message' => '未登录'
+    ]);
+    exit;
+}
