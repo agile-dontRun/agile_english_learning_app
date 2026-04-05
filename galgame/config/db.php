a65fd3919ee4a5aff674aae0b8a28b1cdc6cc3c3
blinder-l <txwd.ch@gmail.com>
2026-04-05 22:57:44 +0800

    Database connection configuration file

---
+++ galgame/config/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = '127.0.0.1';
+$dbname = '';
+$username = '';
+$password = '';
+
+$pdo = new PDO(
+    "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+    $username,
+    $password,
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
